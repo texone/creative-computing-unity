297030ea56e254d6318275c7aa0cc4262038d611
christian riekoff <christian@riekoff.com>
2020-09-24 12:58:38 +0200

    add dynamic heightmap creation

---
+++ util/CCHierarchy.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace cc.creativecomputing.util
+{
+    public class CCHierarchy {
+        
+        public delegate void Apply(GameObject theObject);
+
+        public static void ApplyToChildren(GameObject theObject, Apply theFunction)
+        {
+            if (!theObject) return;
+
+            for (var i = 0; i < theObject.transform.childCount;i++)
+            {
+                theFunction(theObject.transform.GetChild(i).gameObject);
+            }
+        }
+
+        public static void RemoveChildrenPlayMode(GameObject theObject, params string[] theNames)
+        {
+            foreach (var name in theNames)
+            {
+                Object.Destroy(theObject.transform.Find(name).gameObject);
+            }
+        }
+        
+        public static void RemoveChildrenEditor(GameObject theObject, params string[] theNames)
+        {
+            foreach (var name in theNames)
+            {
+                Object.DestroyImmediate(theObject.transform.Find(name).gameObject);
+            }
+        }
+    }
+}
+
